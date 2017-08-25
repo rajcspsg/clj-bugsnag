85222d46d7809ba4414c0bd5e67946df3fbbd830
Csaba Gero <csgero@whitepages.com>
2017-08-25 17:24:38 +0200

    alignment/require cleanup

--- src/clj_bugsnag/core.clj
+++ src/clj_bugsnag/core.clj
@@ -1,29 +1,30 @@
 (ns clj-bugsnag.core
-  (:require [clj-stacktrace.core :refer [parse-exception]]
-            [clj-stacktrace.repl :refer [method-str]]
+  (:require [clj-http.client :as http]
+            [clj-stacktrace
+             [core :refer [parse-exception]]
+             [repl :refer [method-str]]]
+            [clojure
+             [repl :as repl]
+             [string :as string]
+             [walk :as walk]]
             [clojure.java.shell :refer [sh]]
-            [clj-http.client :as http]
-            [environ.core :refer [env]]
-            [clojure.data.json :as json]
-            [clojure.repl :as repl]
-            [clojure.string :as string]
-            [clojure.walk :as walk]))
+            [environ.core :refer [env]]))
 
 (def git-rev
   (delay
-    (try
-      (string/trim (:out (sh "git" "rev-parse" "HEAD")))
-      (catch Exception ex "git revision not available"))))
+   (try
+     (string/trim (:out (sh "git" "rev-parse" "HEAD")))
+     (catch Exception ex "git revision not available"))))
 
 (defn- find-source-snippet
   [around, function-name]
   (try
-    (let [fn-sym (symbol function-name)
-          fn-var (find-var fn-sym)
-          source (repl/source-fn fn-sym)
-          start (-> fn-var meta :line)
+    (let [fn-sym        (symbol function-name)
+          fn-var        (find-var fn-sym)
+          source        (repl/source-fn fn-sym)
+          start         (-> fn-var meta :line)
           indexed-lines (map-indexed (fn [i, line]
-                                        [(+ i start), (string/trimr line)])
+                                       [(+ i start) (string/trimr line)])
                                      (string/split-lines source))]
       (into {} (filter #(<= (- around 3) (first %) (+ around 3)) indexed-lines)))
     (catch Exception ex
@@ -32,21 +33,22 @@
 (defn- transform-stacktrace
   [trace-elems project-ns]
   (try
-    (vec (for [{:keys [file line ns] :as elem} trace-elems
-               :let [project? (string/starts-with? (or ns "_") project-ns)
-                     method (method-str elem)
-                     code (when (string/ends-with? (or file "") ".clj")
-                            (find-source-snippet line (string/replace (or method "") "[fn]" "")))]]
-            {:file file,
-             :lineNumber line,
-             :method method,
-             :inProject project?,
-             :code code}))
+    (vec (for [{:keys [file line ns]
+                :as   elem} trace-elems
+               :let         [project? (string/starts-with? (or ns "_") project-ns)
+                             method   (method-str elem)
+                             code     (when (string/ends-with? (or file "") ".clj")
+                                        (find-source-snippet line (string/replace (or method "") "[fn]" "")))]]
+           {:file       file
+            :lineNumber line
+            :method     method
+            :inProject  project?
+            :code       code}))
     (catch Exception ex
-      [{:file "clj-bugsnag/core.clj",
-        :lineNumber 1,
-        :code {1 (str ex)
-               2 "thrown while building stack trace."}}])))
+      [{:file       "clj-bugsnag/core.clj"
+        :lineNumber 1
+        :code       {1 (str ex)
+                     2 "thrown while building stack trace."}}])))
 
 (defn- stringify
   [thing]
@@ -56,53 +58,53 @@
 
 (defn- unroll [ex project-ns]
   (loop [collected []
-         current ex]
+         current   ex]
     (let [class-name (.getName ^Class (:class current))
           stacktrace (transform-stacktrace (:trace-elems current) project-ns)
-          new-item {:errorClass class-name
-                    :message (:message current)
-                    :stacktrace stacktrace}
-          collected (cons new-item collected)]
+          new-item   {:errorClass class-name
+                      :message    (:message current)
+                      :stacktrace stacktrace}
+          collected  (cons new-item collected)]
       (if-let [next (:cause current)]
         (recur collected next)
         collected))))
 
 (defn exception->json
   [exception options]
-  (let [ex (parse-exception exception)
+  (let [ex         (parse-exception exception)
         class-name (.getName ^Class (:class ex))
         project-ns (get options :project-ns "\000")
-        base-meta (if-let [d (ex-data exception)]
-                    {"ex–data" d}
-                    {})]
-    {:apiKey (:api-key options (env :bugsnag-key))
-     :notifier {:name "clj-bugsnag"
+        base-meta  (if-let [d (ex-data exception)]
+                     {"ex–data" d}
+                     {})]
+    {:apiKey   (:api-key options (env :bugsnag-key))
+     :notifier {:name    "clj-bugsnag"
                 :version "0.3.0"
-                :url "https://github.com/wunderlist/clj-bugsnag"}
-     :events [{:payloadVersion "2"
-               :exceptions (unroll ex project-ns)
-               :context (:context options)
-               :groupingHash (or (:group options)
-                               (if (isa? (type exception) clojure.lang.ExceptionInfo)
-                                 (:message ex)
-                                 class-name))
-               :severity (or (:severity options) "error")
-               :user (:user options)
-               :app {:version (if (contains? options :version)
-                                (:version options)
-                                @git-rev)
-                     :releaseStage (or (:environment options) "production")}
-               :device {:hostname (.. java.net.InetAddress getLocalHost getHostName)}
-               :metaData (walk/postwalk stringify (merge base-meta (:meta options)))}]}))
+                :url     "https://github.com/whitepages/clj-bugsnag"}
+     :events   [{:payloadVersion "2"
+                 :exceptions     (unroll ex project-ns)
+                 :context        (:context options)
+                 :groupingHash   (or (:group options)
+                                     (if (isa? (type exception) clojure.lang.ExceptionInfo)
+                                       (:message ex)
+                                       class-name))
+                 :severity       (or (:severity options) "error")
+                 :user           (:user options)
+                 :app            {:version      (if (contains? options :version)
+                                                  (:version options)
+                                                  @git-rev)
+                                  :releaseStage (or (:environment options) "production")}
+                 :device         {:hostname (.. java.net.InetAddress getLocalHost getHostName)}
+                 :metaData       (walk/postwalk stringify (merge base-meta (:meta options)))}]}))
 
 (defn notify
   "Main interface for manually reporting exceptions.
    When not :api-key is provided in options,
    tries to load BUGSNAG_KEY var from enviroment."
   ([exception]
-    (notify exception nil))
+   (notify exception nil))
   ([exception, options]
-    (let [params (exception->json exception options)
-          url "https://notify.bugsnag.com/"]
-      (http/post url {:form-params params
-                      :content-type :json}))))
+   (let [params (exception->json exception options)
+         url    "https://notify.bugsnag.com/"]
+     (http/post url {:form-params  params
+                     :content-type :json}))))
--- test/clj_bugsnag/core_test.clj
+++ test/clj_bugsnag/core_test.clj
@@ -1,17 +1,17 @@
 (ns clj-bugsnag.core-test
-  (:require [midje.sweet :refer :all]
+  (:require [clj-bugsnag.core :as core]
             [environ.core :refer [env]]
-            [clj-bugsnag.core :as core]))
+            [midje.sweet :refer :all]))
 
 (fact "includes ExceptionInfo's ex-data"
-  (-> (core/exception->json (ex-info "BOOM" {:wat "?!"}) {})
-    :events first (get-in [:metaData "ex–data" ":wat"]))
-  => "?!")
+      (-> (core/exception->json (ex-info "BOOM" {:wat "?!"}) {})
+          :events first (get-in [:metaData "ex–data" ":wat"]))
+      => "?!")
 
 (fact "converts metadata values to strings"
-  (-> (core/exception->json (ex-info "BOOM" {}) {:meta {:reason println}})
-    :events first (get-in [:metaData ":reason"]))
-  => (has-prefix "clojure.core$println@"))
+      (-> (core/exception->json (ex-info "BOOM" {}) {:meta {:reason println}})
+          :events first (get-in [:metaData ":reason"]))
+      => (has-prefix "clojure.core$println@"))
 
 (defn make-crash
   "A function that will crash"
@@ -19,37 +19,37 @@
   (let [closure (fn []
                   (.crash nil))]
 
-  ;;    
+  ;;
   ;; /end to check for 3 lines before and after
 
     (closure)))
 
 (fact "includes source in stack traces"
-  (try
-    (make-crash)
-    (catch Exception ex
-      (-> (core/exception->json ex nil) :events first :exceptions first :stacktrace second :code)
-      => {17 "  \"A function that will crash\""
-          18 "  []"
-          19 "  (let [closure (fn []"
-          20 "                  (.crash nil))]"
-          21 ""
-          22 "  ;;"
-          23 "  ;; /end to check for 3 lines before and after"}
-      (-> (core/exception->json ex nil) :events first :exceptions first :stacktrace (nth 2) :code)
-      => {22 "  ;;"
-          23 "  ;; /end to check for 3 lines before and after"
-          24 ""
-          25 "    (closure)))"})))
+      (try
+        (make-crash)
+        (catch Exception ex
+          (-> (core/exception->json ex nil) :events first :exceptions first :stacktrace second :code)
+          => {17 "  \"A function that will crash\""
+              18 "  []"
+              19 "  (let [closure (fn []"
+              20 "                  (.crash nil))]"
+              21 ""
+              22 "  ;;"
+              23 "  ;; /end to check for 3 lines before and after"}
+          (-> (core/exception->json ex nil) :events first :exceptions first :stacktrace (nth 2) :code)
+          => {22 "  ;;"
+              23 "  ;; /end to check for 3 lines before and after"
+              24 ""
+              25 "    (closure)))"})))
 
 (fact "falls back to BUGSNAG_KEY environment var for :apiKey"
-  (-> (core/exception->json (ex-info "BOOM" {}) {}) :apiKey) => ..bugsnag-key..
-  (provided
-    (env :bugsnag-key) => ..bugsnag-key..))
+      (-> (core/exception->json (ex-info "BOOM" {}) {}) :apiKey) => ..bugsnag-key..
+      (provided
+       (env :bugsnag-key) => ..bugsnag-key..))
 
 (fact "includes nested exceptions"
-  (let [e1 (Exception. "Inner")
-        e2 (Exception. "Middle" e1)
-        e3 (Exception. "Outer"  e2)]
-    (->> (core/exception->json e3 {}) :events first :exceptions (map :message))
-  => ["Inner" "Middle" "Outer"]))
+      (let [e1 (Exception. "Inner")
+            e2 (Exception. "Middle" e1)
+            e3 (Exception. "Outer"  e2)]
+        (->> (core/exception->json e3 {}) :events first :exceptions (map :message))
+        => ["Inner" "Middle" "Outer"]))
